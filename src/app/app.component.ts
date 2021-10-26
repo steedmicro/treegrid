7354dc6e9b01c2bd0383bdf468baceea960e3953
steed924 <steed924@outlook.com>
2021-10-26 15:03:57 +0300

    Create context menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { sampleData } from './jsontreegriddata';
+import { MenuEventArgs, MenuItemModel } from '@syncfusion/ej2-navigations';
+import {
+  TreeGridComponent,
+  PageService,
+} from '@syncfusion/ej2-angular-treegrid';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
 })
-export class AppComponent {
-  title = 'TreeGrid';
+export class AppComponent implements OnInit {
+  public data: Object[] = [];
+  public editSettings: Object = {};
+  public rowHeaderMenuItems: MenuItemModel[] = [];
+  public columnHeaderMenuItems: MenuItemModel[] = [];
+  ngOnInit(): void {
+    this.data = sampleData;
+    this.editSettings = {
+      allowEditing: true,
+      allowAdding: true,
+      allowDeleting: true,
+      mode: 'Row',
+    };
+    this.rowHeaderMenuItems = [
+      { text: 'New', id: 'new_column' },
+      { text: 'Del', id: 'del_column' },
+      { text: 'Edit', id: 'edit_column' },
+      { text: 'Choose', id: 'choose_column' },
+      { text: 'Freeze', id: 'freeze_column' },
+      { text: 'Filter', id: 'filter_column' },
+      { text: 'MultiSort', id: 'multi_sort_column' },
+    ];
+    this.columnHeaderMenuItems = [
+      { text: 'AddNext', id: 'add_next_row' },
+      { text: 'AddChild', id: 'add_child_row' },
+      { text: 'MultiSelect', id: 'multi_select_row' },
+      { text: 'Copy', id: 'copy_row' },
+      { text: 'Cut', id: 'cut_row' },
+      { text: 'PasteNext', id: 'paste_next_row' },
+      { text: 'PasteChild', id: 'paste_child_row' },
+    ];
+  }
+  public menuItemRender(args: MenuEventArgs) {
+    if (args.item.id === 'freeze_column') {
+      args.element.innerHTML =
+        '<span class="e-frame e-icons e-check"></span> Freeze';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,86 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { SparklineAllModule } from '@syncfusion/ej2-angular-charts';
 
+import { DialogModule } from '@syncfusion/ej2-angular-popups';
+
+import {
+  DropDownListAllModule,
+  MultiSelectAllModule,
+} from '@syncfusion/ej2-angular-dropdowns';
+
+import {
+  ToolbarModule,
+  ToolbarAllModule,
+} from '@syncfusion/ej2-angular-navigations';
+
+import {
+  ButtonAllModule,
+  CheckBoxAllModule,
+  SwitchModule,
+} from '@syncfusion/ej2-angular-buttons';
+
+import {
+  DatePickerModule,
+  DatePickerAllModule,
+} from '@syncfusion/ej2-angular-calendars';
+
+import { NumericTextBoxAllModule } from '@syncfusion/ej2-angular-inputs';
+
+import { TreeGridAllModule } from '@syncfusion/ej2-angular-treegrid';
+import { ContextMenuModule } from '@syncfusion/ej2-angular-navigations';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {
+  PageService,
+  FilterService,
+  EditService,
+  ToolbarService,
+  SortService,
+  ResizeService,
+  ReorderService,
+  ExcelExportService,
+  PdfExportService,
+  ContextMenuService,
+} from '@syncfusion/ej2-angular-treegrid';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    CommonModule,
+    HttpModule,
+    TreeGridAllModule,
+    NumericTextBoxAllModule,
+    ToolbarModule,
+    DropDownListAllModule,
+    ButtonAllModule,
+    DialogModule,
+    MultiSelectAllModule,
+    CheckBoxAllModule,
+    ReactiveFormsModule,
+    FormsModule,
+    DatePickerModule,
+    SparklineAllModule,
+    BrowserModule,
+    SwitchModule,
+    ContextMenuModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    PageService,
+    SortService,
+    FilterService,
+    EditService,
+    SortService,
+    ResizeService,
+    ReorderService,
+    ExcelExportService,
+    PdfExportService,
+    ContextMenuService,
+    ToolbarService,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
